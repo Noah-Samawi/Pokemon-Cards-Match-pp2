b438f5617f46f1b395fbd66b96dcebc22bda1e46
Noah-Samawi <>
2023-12-30 05:08:47 +0000

    Add shuffle card cardset und random index

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,16 @@
 function shuffleCards() {
 
-
+    cardSet = cardList.concat(cardList); //Git the two of each card
+    console.log(cardSet);
+    //shuffle card
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get the random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
 }
      
 function startGame() {
@@ -12,5 +22,5 @@ function hideCards() {
 }
 
 function selectCard() {
-    
+
 }
